74c00822b9242bb559b817ab48448722b3cf57d3
Mo Tarbin <mhed.t91@gmail.com>
2024-07-01 02:12:05 -0400

    Update API URL in Config.js for development environment

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
-export const API_URL =
-  import.meta.env.VITE_APP_API_URL || 'http://localhost:2021'
+export const API_URL = import.meta.env.VITE_APP_API_URL // || 'http://localhost:2021'
 export const REDIRECT_URL = import.meta.env.VITE_APP_REDIRECT_URL //|| 'http://localhost:3000'
 export const GOOGLE_CLIENT_ID = import.meta.env.VITE_APP_GOOGLE_CLIENT_ID
 export const ENVIROMENT = import.meta.env.VITE_APP_ENVIROMENT
