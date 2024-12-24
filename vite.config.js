08085ee88600f59a683678df2ce167d4cf18b605
majicmaj <majd.tarbin@outlook.com>
2024-12-24 13:28:49 -0500

    chore: Update API URL in development environment and configure Workbox to not use cache for updates

--- vite.config.js
+++ vite.config.js
@@ -61,6 +61,7 @@ export default defineConfig({
       workbox: {
         skipWaiting: true, // Force the waiting service worker to become the active service worker
         clientsClaim: true, // Take control of uncontrolled clients as soon as the service worker becomes active
+        updateViaCache: 'none', // Do not use the cache for the update process
       },
     }),
   ],
