47571fe092c915119ed700e437f616377f86e815
Mo Tarbin <mhed.t91@gmail.com>
2024-12-26 02:30:56 -0500

    Add external dependency configuration for Rollup in Vite build

--- vite.config.js
+++ vite.config.js
@@ -64,6 +64,12 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    rollupOptions: {
+      external: [
+        '@rollup/rollup-linux-x64-gnu'
+      ]
+    },
 
   resolve: {
     alias: [
