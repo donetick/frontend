778930015e8f8372bb6abda4048cda90133eff94
Mo Tarbin <mhed.t91@gmail.com>
2025-06-18 00:24:28 -0400

    refactor: Remove debug log from useSSEContext hook

--- src/hooks/useSSEContext.js
+++ src/hooks/useSSEContext.js
@@ -2,6 +2,5 @@ import { useContext } from 'react'
 import { SSEContext } from '../contexts/SSEContext'
 
 export const useSSEContext = () => {
-  console.log('=== useSSEContext called ===')
   return useContext(SSEContext)
 }
