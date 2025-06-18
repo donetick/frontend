b4652e248687bf32c5ad0821330a68279020f9f6
Mo Tarbin <mhed.t91@gmail.com>
2025-06-18 01:19:52 -0400

    feat: Add animation components and global styles for improved UX

---
+++ src/components/animations/index.js
@@ -0,0 +1,11 @@
+// Animation Components
+export { default as PageTransition } from './PageTransition'
+export { default as LoadingScreen } from './LoadingScreen'
+export { default as SkeletonLoader } from './SkeletonLoader'
+export { default as SmoothButton } from './SmoothButton'
+export { default as SmoothCard } from './SmoothCard'
+export { default as AnimatedList } from './AnimatedList'
+export { default as StaggeredList } from './StaggeredList'
+
+// Animation Styles
+import './PageTransition.css'
