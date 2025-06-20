0b7097082d1687347b4c748976e388452331a718
Mo Tarbin <mhed.t91@gmail.com>
2025-06-20 00:38:27 -0400

    refactor: Update export order of animation components for consistency

--- src/components/animations/index.js
+++ src/components/animations/index.js
@@ -1,10 +1,9 @@
 // Animation Components
-export { default as PageTransition } from './PageTransition'
+export { default as AnimatedList } from './AnimatedList'
 export { default as LoadingScreen } from './LoadingScreen'
-export { default as SkeletonLoader } from './SkeletonLoader'
+export { default as PageTransition } from './PageTransition'
 export { default as SmoothButton } from './SmoothButton'
 export { default as SmoothCard } from './SmoothCard'
-export { default as AnimatedList } from './AnimatedList'
 export { default as StaggeredList } from './StaggeredList'
 
 // Animation Styles
