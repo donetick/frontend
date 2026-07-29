42537ba88862856403a4e738ec88b65b36559738
cm-moment <50913840+cm-moment@users.noreply.github.com>
2026-07-29 17:32:48 +0800

    Update LocalizationContext.jsx
    add 'zh‑CN', name: 'Chinese(Simplified)', nativeName: '简体中文'

--- src/contexts/LocalizationContext.jsx
+++ src/contexts/LocalizationContext.jsx
@@ -28,7 +28,7 @@ export const AVAILABLE_LANGUAGES = [
   { code: 'nl', name: 'Dutch', nativeName: 'Nederlands' },
   { code: 'ja', name: 'Japanese', nativeName: '日本語' },
   { code: 'pt', name: 'Portuguese (Brazil)', nativeName: 'Português (Brasil)' },
-  { code: 'ja', name: 'Japanese', nativeName: '日本語' },
+  { code: 'zh‑CN', name: 'Chinese(Simplified)', nativeName: '简体中文' },
 ]
 
 export const LocalizationProvider = ({ children }) => {
