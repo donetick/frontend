09f9ed9e2026dd18d976db4a82114725994b0647
Mo Tarbin <mhed.t91@gmail.com>
2026-08-02 16:33:35 -0400

    Add support for in app feedback

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,12 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import pkg from './package.json'
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(pkg.version),
+  },
   plugins: [
     react(),
     VitePWA({
