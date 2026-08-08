b1a09082f772295899bf02009c6cb02ccf86d915
Mo Tarbin <mhed.t91@gmail.com>
2026-08-07 21:55:59 -0400

    Add theme background support for light and dark modes in StatusBarManager and ThemeContext Fix: https://github.com/donetick/donetick/issues/759

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -5,3 +5,8 @@ import tailwindConfig from '/tailwind.config.mjs'
 export const { theme: THEME } = resolveConfig(tailwindConfig)
 
 export const COLORS = THEME.colors
+
+export const THEME_BACKGROUND = {
+  dark: '#000000',
+  light: '#FFFFFF',
+}
